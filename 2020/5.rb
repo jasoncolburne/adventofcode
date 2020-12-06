6eaa217b2e12099c79b213712bd078e7aa3e3a8f
Jason Colburne <j.colburne@gmail.com>
2020-12-06 00:29:07 -0500

    solutions for day 5 and 6

---
+++ 2020/5.rb
@@ -0,0 +1,42 @@
+#!env ruby
+
+require 'pp'
+require './screen'
+
+data = <<EOT
+EOT
+
+data = <<EOT
+EOT
+
+data = <<EOT
+FBFBBFFRLR
+EOT
+
+data = File.read(ARGV[0])
+
+lines = data.chomp.split("\n")
+
+seat_ids = (lines.map do |line|
+  row = line[0..6].split('').map { |c| c == 'F' ? '0' : '1' }.join('').to_i(2)
+  seat = line[7..].split('').map { |c| c == 'L' ? '0' : '1' }.join('').to_i(2)
+  row * 8 + seat
+end)
+
+puts seat_ids.max
+
+pp seat_ids.sort.first(10)
+
+last_id = nil
+seat_ids.sort.each do |id|
+  if last_id
+    if id - 1 != last_id
+      puts id - 1
+      exit
+    else
+      last_id = id
+    end
+  else
+    last_id = id
+  end
+end
---
+++ 2020/6.rb
@@ -0,0 +1,64 @@
+#!env ruby
+
+require 'pp'
+require './screen'
+
+data = <<EOT
+EOT
+
+data = <<EOT
+EOT
+
+data = <<EOT
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+EOT
+
+data = File.read(ARGV[0])
+
+lines = data.chomp.split("\n")
+
+groups = []
+current_group = []
+lines.each do |line|
+  if line.empty?
+    groups << current_group
+    current_group = []
+  else
+    current_group = (current_group + line.split('')).uniq
+  end
+end
+groups << current_group
+
+puts groups.map { |group| group.count }.inject(&:+)
+
+groups = []
+current_group = []
+new_group = true
+lines.each do |line|
+  if line.empty?
+    groups << current_group
+    current_group = []
+    new_group = true
+  else
+    current_group = new_group ? line.split('').uniq : (line.split('').uniq.select { |char| current_group.include?(char) })
+    new_group = false
+  end
+end
+groups << current_group
+
+puts groups.map { |group| group.count }.inject(&:+)
+
