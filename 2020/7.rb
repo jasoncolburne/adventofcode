e0b4a808b70a748e4e2c95a9c764d1b2481cc34e
Jason Colburne <j.colburne@gmail.com>
2020-12-07 03:38:54 -0500

    reorder for clarity

--- 2020/7.rb
+++ 2020/7.rb
@@ -44,10 +44,10 @@ def can_hold(target, holder, contains)
   contains[holder].keys.include?(target) || contains[holder].keys.any? { |new_holder| can_hold(target, new_holder, contains) }
 end
 
+pp contains.keys.select { |key| can_hold('shiny gold', key, contains) }.count
+
 def sum_bags(target, contains)
   1 + contains[target].map { |inner, count| count * sum_bags(inner, contains) }.inject(0, :+)
 end
-
-pp contains.keys.select { |key| can_hold('shiny gold', key, contains) }.count
 
 pp sum_bags('shiny gold', contains) - 1
