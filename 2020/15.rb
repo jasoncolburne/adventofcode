85a18c10da43f8aeef76f3fec09374a3ad65f332
Jason Colburne <j.colburne@gmail.com>
2020-12-15 01:04:12 -0500

    day 15

--- 2020/15.rb
+++ 2020/15.rb
@@ -1,18 +1,32 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
-require 'bundler/setup'
-require 'jason/math'
-require 'pp'
-require 'set'
-require './screen'
+data = [10,16,6,0,1,17]
+#data = [1,3,2]
+#data = [0,3,6]
 
-data = <<EOT
-EOT
+# i did the main loop in a confusing way, i think.
+def number_spoken(initial_numbers, n)
+  spoken = {}
+  last_spoken = nil
+  initial_numbers.each_with_index do |number, i|
+    spoken[number] = i
+    last_spoken = number
+  end
 
-data = <<EOT
-EOT
+  last_spoken_first = true
+  ((initial_numbers.count)..(n - 1)).each do |i|
+    next_spoken = if last_spoken_first
+      0
+    else
+      i - spoken[last_spoken] - 1
+    end
+    spoken[last_spoken] = i - 1
 
-data = File.read(ARGV[0])
+    last_spoken_first = !spoken.include?(next_spoken)
+    last_spoken = next_spoken
+  end
 
-lines = data.chomp.split("\n")
+  last_spoken
+end
+
+puts number_spoken(data, ARGV[0].to_i)
