9121bca75acd23dca14f8d283792a05e56575b7b
Jason Colburne <j.colburne@gmail.com>
2020-12-17 22:34:48 -0500

    abstract neighbours method

--- 2020/17.rb
+++ 2020/17.rb
@@ -8,39 +8,43 @@ EOT
 
 data = File.read(ARGV[0])
 
-neighbours_3d = ->(cell) do
-  ((cell[0] - 1)..(cell[0] + 1)).map do |x|
-    ((cell[1] - 1)..(cell[1] + 1)).map do |y|
-      ((cell[2] - 1)..(cell[2] + 1)).map do |z|
-        neighbour = [x, y, z]
-        cell == neighbour ? nil : neighbour
-      end
-    end
-  end.flatten(2).compact
+# neighbours_3d = ->(cell) do
+#   ((cell[0] - 1)..(cell[0] + 1)).map do |x|
+#     ((cell[1] - 1)..(cell[1] + 1)).map do |y|
+#       ((cell[2] - 1)..(cell[2] + 1)).map do |z|
+#         neighbour = [x, y, z]
+#         cell == neighbour ? nil : neighbour
+#       end
+#     end
+#   end.flatten(2).compact
+# end
+
+def neighbours(cell, dimensions = nil)
+  d = dimensions || cell.count
+
+  string = ""
+  if d.zero?
+    coordinates = (0..(cell.count - 1)).map { |n| "x#{n}" }.join(", ")
+    string += "neighbour = [#{coordinates}]\n"
+    string += "neighbour == cell ? nil : neighbour\n"
+  else
+    string += "((cell[#{d - 1}] - 1)..(cell[#{d - 1}] + 1)).map do |x#{d - 1}|\n"
+    string += neighbours(cell, d - 1)
+    string += "end\n"
+  end
+
+  dimensions == nil ? eval(string.chomp + ".flatten(#{d - 1}).compact\n") : string
 end
 
-neighbours_4d = ->(cell) do
-  ((cell[0] - 1)..(cell[0] + 1)).map do |x|
-    ((cell[1] - 1)..(cell[1] + 1)).map do |y|
-      ((cell[2] - 1)..(cell[2] + 1)).map do |z|
-        ((cell[3] - 1)..(cell[3] + 1)).map do |w|
-          neighbour = [x, y, z, w]
-          cell == neighbour ? nil : neighbour
-        end
-      end
-    end
-  end.flatten(3).compact
-end
-
-def evolve(state, neighbours_lambda, n = 6)
+def evolve(state, n = 6)
   n.times do
     # could use a set here if memory was a concern - not sure if the uniq is overall slower than
     # the set operations would be but it probably is too. this solution still runs in a few seconds
-    to_check = state.select { |k, v| v }.keys.map { |cell| neighbours_lambda[cell] + [cell] }.flatten(1).uniq
+    to_check = state.select { |k, v| v }.keys.map { |cell| neighbours(cell) + [cell] }.flatten(1).uniq
     new_state = {}
 
     to_check.each do |cell|
-      count = neighbours_lambda[cell].select { |c| state[c] }.count
+      count = neighbours(cell).select { |c| state[c] }.count
       if state[cell]
         new_state[cell] = count == 2 || count == 3 ? true : false
       else
@@ -66,5 +70,5 @@ lines.each_with_index do |line, y|
   end
 end
 
-puts evolve(state_3d, neighbours_3d).select { |k, v| v }.count
-puts evolve(state_4d, neighbours_4d).select { |k, v| v }.count
+puts evolve(state_3d).select { |k, v| v }.count
+puts evolve(state_4d).select { |k, v| v }.count
