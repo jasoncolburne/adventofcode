a9eaa0d47ec38297597d892f0d45e90314005632
Jason Colburne <j.colburne@gmail.com>
2020-12-24 01:27:25 -0500

    day 25 prep

---
+++ 2020/25.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'matrix'
+require 'digest'
+require 'rubygems'
+require 'bundler/setup'
+require 'jason/math'
+require 'pp'
+require 'set'
+require './screen'
+
+data = <<EOT
+EOT
+
+data = File.read(ARGV[0])
+
+lines = data.chomp.split("\n")
