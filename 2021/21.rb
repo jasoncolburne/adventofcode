13904d948d9648f5f23a5beb0576eb27e5ee1c57
Jason Colburne <j.colburne@gmail.com>
2021-12-21 08:20:39 -0400

    cleanup

--- 2021/21.rb
+++ 2021/21.rb
@@ -19,15 +19,11 @@ p1_start = lines.first.split(': ').last.to_i
 p2_start = lines.last.split(': ').last.to_i
 
 class Game
-  attr_reader :rolls
-  attr_reader :p1_score
-  attr_reader :p2_score
+  attr_reader :rolls, :scores
 
   def initialize(p1_start, p2_start)
-    @p1_position = p1_start - 1
-    @p2_position = p2_start - 1
-    @p1_score = 0
-    @p2_score = 0
+    @positions = [p1_start - 1, p2_start - 1]
+    @scores = [0, 0]
 
     @winning_score = 1000
     @rolls = 0
@@ -42,11 +38,13 @@ class Game
   end
 
   def advance_until_done
+    player = 0
     loop do
-      move(1)
-      return if @p1_score >= @winning_score
-      move(2)
-      return if @p2_score >= @winning_score
+      move(player)
+      return if @scores[player] >= @winning_score
+
+      player += 1
+      player %= 2
     end
   end
 
@@ -59,22 +57,15 @@ class Game
   end
 
   def move(player)
-    case player
-    when 1
-      @p1_position += sum_triple_roll
-      @p1_position %= 10
-      @p1_score += @p1_position + 1
-    when 2
-      @p2_position += sum_triple_roll
-      @p2_position %= 10
-      @p2_score += @p2_position + 1
-    end
+    @positions[player] += sum_triple_roll
+    @positions[player] %= 10
+    @scores[player] += @positions[player] + 1
   end
 end
 
 game = Game.new(p1_start, p2_start)
 game.advance_until_done
-p game.rolls * [game.p1_score, game.p2_score].min
+p game.rolls * game.scores.min
 
 all_sums = (1..3).map { |a| (1..3).map { |b| (1..3).map { |c| a + b + c } } }.flatten
 counted_sums = Hash.new(0)
